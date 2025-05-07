4d40d3f4a37bfb33ca188a2b2b30fa02f77bfd66
Viet Hoang <viethoang@RavensMac.local>
2025-05-07 22:09:59 +0700

    Implemented Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Prompt from './Components/Prompt'
+import { Hero } from './Components/Hero'
 
 export const App = () => {
   return (
     <div>
+      <Prompt/>
+
       <Navbar/>
+
+      <Hero/>
     </div>
   )
 }
