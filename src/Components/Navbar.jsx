e6321378b688cb5a2816fc9f22541828226fef16
theRaven1312 <rothraven1312@gmail.com>
2025-05-07 17:13:54 +0700

    Create project and installed navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import arrowDown from '../../public/Assets/arrowdown.svg'
+import searchIcon from '../../public/Assets/searchIcon.svg'
+import cartIcon from '../../public/Assets/cart.svg'
+import profile from '../../public/Assets/profile.svg'
+
+const Navbar = () => {
+  return (
+    <div className = 'navbar'>
+        <div className = 'navbarLogo'>T3.SAHUR</div>
+        <ul className = 'navbarList'>
+            <li className = 'navbarListSub'>
+                <span>Shop</span>
+                <img src ={arrowDown}/>
+            </li>
+            <li>On Sale</li>
+            <li>New Arrivals</li>
+            <li>Categories</li>
+        </ul>
+        <div className='navbarSearch'>
+            <img src={searchIcon} alt="search" />
+            <input type="text" placeholder='Search for products' />
+        </div>
+        <div className = 'navbarCartProfile'>
+            <img src = {cartIcon} />
+            <img src = {profile} />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
