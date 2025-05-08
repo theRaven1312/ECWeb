7de4b7265c6346caa63c85295be3a2e78406fd95
Viet Hoang <viethoang@RavensMac.local>
2025-05-08 20:12:36 +0700

    Implemented Product Display Section pt1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import Prompt from './Components/Prompt'
 import { Hero } from './Components/Hero'
+import BlackStripe from './Components/BlackStripe'
+import FourItemDisplay from './Components/FourItemDisplay'
 
 export const App = () => {
   return (
@@ -11,6 +13,14 @@ export const App = () => {
       <Navbar/>
 
       <Hero/>
+
+      <BlackStripe/>
+
+      <div className ='DisplaySection'>
+          <FourItemDisplay heading='New Arrivals'/>
+          <div className="w-[90%] h-px bg-gray-300"></div>
+          <FourItemDisplay heading='Top Selling'/>
+        </div>
     </div>
   )
 }
