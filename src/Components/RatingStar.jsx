52fd13b586ad09871284aa310765ce3b0cbdc900
theRaven1312 <rothraven1312@gmail.com>
2025-05-09 10:33:49 +0700

    Add Rating Star component

---
+++ src/Components/RatingStar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import star from '../../public/Assets/star.svg'
+
+const RatingStar = () => {
+  return (
+    <div className='ratingStar'>
+        <img src={star}/>
+        <img src={star}/>
+        <img src={star}/>
+        <img src={star}/>
+        <img src={star}/>
+    </div>
+  )
+}
+
+export default RatingStar
