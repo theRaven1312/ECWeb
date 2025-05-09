f7d6f95378bd47a179b6e48eb62c8e67ae31f758
Viet Hoang <viethoang@RavensMac.local>
2025-05-09 15:29:25 +0700

    What is going on

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import HomePage from './pages/HomePage'
+import ProductionPage from './pages/ProductionPage'
 
 export const App = () => {
   return (
-    <HomePage/>
+    <ProductionPage/>
   )
 }
 
