8a4c983b280424af0c8066e393b0bb6c61a88cf4
theRaven1312 <rothraven1312@gmail.com>
2025-05-09 11:23:09 +0700

    Add directory pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,9 @@
 import React from 'react'
-import Navbar from './Components/Navbar'
-import Prompt from './Components/Prompt'
-import { Hero } from './Components/Hero'
-import BlackStripe from './Components/BlackStripe'
-import FourItemDisplay from './Components/FourItemDisplay'
-import GalleryDisplay from './Components/GalleryDisplay'
-import CommentDisplay from './Components/CommentDisplay'
-import Footer from './Components/Footer'
+import HomePage from './pages/HomePage'
 
 export const App = () => {
   return (
-    <div>
-      <Prompt/>
-
-      <Navbar/>
-
-      <Hero/>
-
-      <BlackStripe/>
-
-      <div className ='displaySection'>
-          <FourItemDisplay heading='New Arrivals'/>
-          <div className="w-[90%] h-px bg-gray-300"></div>
-          <FourItemDisplay heading='Top Selling'/>
-          <GalleryDisplay/>
-        </div>
-        
-      <CommentDisplay/>
-
-      <Footer/>
-    </div>
-
-   
+    <HomePage/>
   )
 }
 
