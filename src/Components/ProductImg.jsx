b9e5855cebc3b383baa79feec6b5069fb8f1cd9a
theRaven1312 <rothraven1312@gmail.com>
2025-05-09 15:29:59 +0700

    What the hell

---
+++ src/Components/ProductImg.jsx
@@ -0,0 +1,37 @@
+import {useState} from "react";
+import ProductImg1 from "../../public/Assets/ProductionAssets/t-shirt1.png";
+import ProductImg2 from "../../public/Assets/ProductionAssets/t-shirt2.png";
+import ProductImg3 from "../../public/Assets/ProductionAssets/t-shirt3.png";
+
+const thumbnails = [ProductImg1, ProductImg2, ProductImg3];
+
+const ProductImg = () => {
+    const [selectedImage, setSelectedImage] = useState(thumbnails[0]);
+
+    return (
+        <div className="productImg-container">
+            {thumbnails.map((img, i) => (
+                <img
+                    key={i}
+                    src={img}
+                    onClick={() => setSelectedImage(img)}
+                    className={`img-left  ${
+                        img === selectedImage
+                            ? "border-black border-2 "
+                            : "border-transparent opacity-50"
+                    }`}
+                    alt="thumb"
+                />
+            ))}
+            <div className="img-right ">
+                <img
+                    src={selectedImage}
+                    className="rounded-2xl max-w-md"
+                    alt="main"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default ProductImg;
---
+++ src/pages/ProductionPage.jsx
@@ -0,0 +1,68 @@
+import Prompt from "../Components/Prompt.jsx";
+import Navbar from "../Components/Navbar.jsx";
+import MainContainer from "../Components/MainContainer.jsx";
+import DirectLink from "../Components/DirectLink.jsx";
+import ProductImg from "../Components/ProductImg.jsx";
+import ProductInfo from "../Components/ProductInfo.jsx";
+import CommentSelector from "../Components/CommentSelector.jsx";
+import CommentDisplay from "../Components/CommentDisplay.jsx";
+import FourItemDisplay from "../Components/FourItemDisplay.jsx";
+import Footer from "../Components/Footer.jsx";
+
+export default function ProductionPage() {
+    return (
+        <>
+            <Prompt></Prompt>
+
+            <MainContainer>
+                <Navbar className="navbar border-b-2 border-[var(--unactive-btn)]"></Navbar>
+                <ol className="direction">
+                    <a href="#!">Home</a>
+                    <DirectLink link="Shop" />
+                    <DirectLink link="Men" />
+                    <DirectLink link="T-Shirt" linkClassName="activeLink" />
+                </ol>
+                {/* ProductDetail */}
+
+                <div className="product-container">
+                    <div className="product-img">
+                        <ProductImg />
+                    </div>
+                    <div className="product-content ">
+                        <ProductInfo />
+                    </div>
+                </div>
+
+                {/* Comment-Section */}
+
+                <CommentSelector />
+                <div className="comment-header">
+                    <div className="flex items-center">
+                        <h1 className="comment-heading">All reviews</h1>
+                        <span className="comment-quanity">(451)</span>
+                    </div>
+                    <div className="flex">
+                        <button className="primary-btn comment-header__adjustBtn">
+                            <i class="fa-solid fa-sliders "></i>
+                        </button>
+                        <div className="comment-header__moreBtn">
+                            <span>Latest</span>
+                            <button className="w-[16px] h-[16px] cursor-pointer flex-center ml-2 hover:bg-gray-100 rounded-full">
+                                <i class="fa-solid fa-angle-down "></i>
+                            </button>
+                        </div>
+                        <button className="primary-btn comment-header__writeBtn">
+                            Write a preview
+                        </button>
+                    </div>
+                </div>
+                <CommentDisplay />
+                <button className="primary-btn learnMoreBtn">
+                    Learn More Reviews
+                </button>
+                <FourItemDisplay />
+            </MainContainer>
+            <Footer />
+        </>
+    );
+}
