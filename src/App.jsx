2534a819c7d7ed42c1e990a7a6d8763a22b8e93f
theRaven1312 <rothraven1312@gmail.com>
2025-05-19 17:03:42 +0700

    Fix: Nhảy đến đầu trang mới

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const AppContent = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/products" element={<ProductionPage />} />
+        <Route path="/product" element={<ProductionPage />} />
         <Route path="/category" element={<CategoryPage />} />
         <Route path="/cart" element={<CartPage />} />
       </Routes>
--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import star from "../../public/Assets/star.svg";
 import RatingStar from "./RatingStar";
+import {Link} from 'react-router-dom';
+
 const ProductCard = () => {
     return (
-        <div className="productCard">
-            <div className="productImg w-64 h-64 bg-gray-400"></div>
-            <div className="productName">Some product</div>
-            <div className="productRating">
-                <div className="ratingStar">
-                    <RatingStar />
+        <Link to = '/product'>
+            <div className="productCard" onclick={window.scrollTo(0,0)}>
+                <div className="productImg w-64 h-64 bg-gray-400"></div>
+                <div className="productName">Some product</div>
+                <div className="productRating">
+                    <div className="ratingStar">
+                        <RatingStar />
+                    </div>
+                    <div className="ratingNumber">4/5</div>
                 </div>
-                <div className="ratingNumber">4/5</div>
+                <div className="productPrice">$200</div>
             </div>
-            <div className="productPrice">$200</div>
-        </div>
+        </Link>
     );
 };
 
