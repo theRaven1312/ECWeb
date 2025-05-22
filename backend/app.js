d6a00f20532ddaa0d28d5429e7d9b916ef84075d
theRaven1312 <rothraven1312@gmail.com>
2025-05-22 18:36:11 +0700

    Minor change

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-app.use(`${api}/user`, userRouter);
+app.use(`${api}/users`, userRouter);
 app.use(`${api}/products`, productRouter);
 
 app.listen(port, () => {
