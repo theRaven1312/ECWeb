230654d81d64069c9eae2924c79235209ad15976
b1endong <tranhoanghai260805@gmail.com>
2025-05-23 12:46:20 +0700

    feature/ Phân quyền người dùng

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
 import userController from "../controllers/user.controller.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 router.post("/sign-in", userController.createUser);
 router.post("/log-in", userController.logInUser);
 router.put("/update-user/:id", userController.updateUser); //từ admin, tên sai
-router.delete("/delete-user/:id", userController.deleteUser);
+router.delete("/delete-user/:id", authMiddleware, userController.deleteUser);
 
 //+ 1 hàm người dùng tự update
 
