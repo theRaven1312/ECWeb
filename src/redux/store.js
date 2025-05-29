0c1f560f4200d3cc41856d16fffe20e330813c74
b1endong <tranhoanghai260805@gmail.com>
2025-05-29 22:41:57 +0700

    small fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import {configureStore} from "@reduxjs/toolkit";
 import userReducer from "../redux/UserSliceRedux.js";
-import cartReducer from "../redux/CartSliceRedux.js";
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
-        cart: cartReducer,
     },
 });
